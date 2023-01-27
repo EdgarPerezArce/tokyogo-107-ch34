8fae35805e289e85c302e0d9de7302ee13c7695c
EdgarPerezArce <edgarperezarce@gmail.com>
2023-01-26 19:33:55 -0800

    tokyogo 107 ch34

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+    return (
+        <div className="footer">
+            <label>Edgar Perez</label>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+
+import "./navbar.css";
+
+function NavBar() {
+    return (
+        <div className='navbar'>
+            menu here
+        </div>
+    );
+}
+
+export default NavBar;
