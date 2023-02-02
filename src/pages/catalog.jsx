9f31ddad33cbf3453427eaa0f9b3b044b1bc8d27
EdgarPerezArce <edgarperezarce@gmail.com>
2023-02-02 14:01:40 -0800

    107 tokyogo ch34 commit 4

--- src/pages/catalog.jsx
+++ src/pages/catalog.jsx
@@ -28,10 +28,10 @@ function Catalog() {
     }
 
     return (
-        <div>
+        <div class="productheader">
             <h1>Check out The goods</h1>
                 <h5>We have {products.length} products ready for you!</h5>
-                <button onClick={magicTest}>Magic</button>
+                <button class="btn btn-warning" onClick={magicTest}>Magic</button>
                 <br />
 
             {products.map( p => <Product data={p}></Product>)}
